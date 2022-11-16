e7bb523ab083a8860f544e82d7f47edaab3c2fbd
Douglas <douglas.silva12@alunos.sc.senac.br>
2022-11-16 16:21:50 -0300

    ok

---
+++ aula20221110/index.php
@@ -0,0 +1,31 @@
+<h1> Ola teste</h1>
+
+<!-- trecho de código php -->
+<?php // abre o código
+
+// forma 1
+$estilo_css = '<style>.div-dade{border:1px solid green;}</style>';
+$idade = 35;
+echo '<div style="color:red;" class="div-idade">
+<h1>Minha idade é: </h1> <h1>'. $idade;"</h1></div>";
+
+/**
+ * para várias linhas de comentários faz /**
+ */
+
+// 2 barras faz a linha ser comentário
+
+// variável com nome minúsculo
+
+//início de fonção com nome minúsculo
+
+// fecha o código
+echo '<br>exemplo de var_dump:<br>';
+var_dump($idade);
+
+echo '<br><br><br><br>';
+
+echo '<pre>' .print_r($cor,true)
+?>
+
+<h2>Ola teste outro html</h2>
