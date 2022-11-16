842e1e9a534176c9faa286c1748717965522098c
Douglas <douglas.silva12@alunos.sc.senac.br>
2022-11-16 17:44:18 -0300

    ok

---
+++ Atividades/atividade_04/exercicio05.php
@@ -0,0 +1,7 @@
+<?php
+
+$cor = Array(1 => 'vermelho', 2 => 'verde', 3 => 'branca', 4 => 'preto');
+
+
+echo '"A memória daquela cena para mim é como um quadro do filme para sempre congelada naquele momento: 
+o tapete ' . $cor[1] . ', o gramado ' . $cor[2] . ', a casa ' . $cor[3] . ', o céu ' . $cor[4] . '. O novo presidente e sua primeira-dama." - Richard M. Nixon';
